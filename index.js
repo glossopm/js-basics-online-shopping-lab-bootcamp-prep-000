5bfdb4f06065d33d8847defecba56253eff239cf
Malik Glossop <malik.glossop@gmail.com>
2018-08-11 15:51:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    var string = `Your total is ${total()}, which will be charged to the card ${cardNumber}.`
+    var string = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     cart = []
     return string
   }
