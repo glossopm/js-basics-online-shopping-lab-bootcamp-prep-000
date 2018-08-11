c66afc0fa20e188be2f10df695db823922c0bbe1
Malik Glossop <malik.glossop@gmail.com>
2018-08-11 15:18:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var item_object = {itemName: item, itemPrice: Math.floor((Math.random() *100) +1)}
+ cart.push(item_object)
+ return `${item} has been added to your cart.`
 }
-
+addToCart("cake")
+addToCart("cheese")
 function viewCart() {
-  // write your code here
+  if (cart.length > 0) {
+    var names = {}
+    var contents = ""
+    for (var i = 0; i < cart.length; ++i) {
+      names = cart[i]
+      contents += `${names.itemName}at $${names.itemPrice}, and`
+    }
+    contents = contents.slice(0, -2)
+    return `In your cart, you have ${contents}.`
+  }
+  else {
+    return "Your shopping cart is empty."
+  }
 }
-
+viewCart()
 function total() {
   // write your code here
 }
